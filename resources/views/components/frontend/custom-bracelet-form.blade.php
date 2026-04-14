4126dea6ef603c7a37ba382adeeb4e04d763ef29
Yashvir Pal <yashvir.pal@kalkine.co.in>
2026-04-14 09:53:38 +0530

    form value remove custom bracelate

--- resources/views/components/frontend/custom-bracelet-form.blade.php
+++ resources/views/components/frontend/custom-bracelet-form.blade.php
@@ -5,25 +5,25 @@
             <div class="col-md-6">
                 <div class="input-group">
                     <label for="cb_name">Name</label>
-                    <input type="text" name="cb_name" id="cb_name" value="Test User"/>
+                    <input type="text" name="cb_name" id="cb_name" value="" />
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="input-group">
                     <label for="cb_dob">Date Of Birth</label>
-                    <input type="date" name="cb_dob" id="cb_dob" value="02-04-2026"/>
+                    <input type="date" name="cb_dob" id="cb_dob" value="" />
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="input-group">
                     <label for="cb_problem">Problem</label>
-                    <input type="text" name="cb_problem" id="cb_problem" value="Test Problem"/>
+                    <input type="text" name="cb_problem" id="cb_problem" value="" />
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="input-group">
                     <label for="cb_specify_problem">Specific Problem</label>
-                    <textarea name="cb_specify_problem" id="cb_specify_problem">Asdfasddddddddddddddddddddddddddd</textarea>
+                    <textarea name="cb_specify_problem" id="cb_specify_problem"></textarea>
                 </div>
             </div>
         </div>
